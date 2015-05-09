564deaa3c94dade67061df1fe9084f38045a8e54
Yukai Huang <yukaihuang1993@hotmail.com>
2015-05-09 13:48:22 +0800

    chinese webpage / normalize periods

--- crawler.rb
+++ crawler.rb
@@ -4,8 +4,11 @@ require 'json'
 require 'rest-client'
 
 courses = []
-(1..46).each do |page_number|
+r = RestClient.get "https://course.ncu.edu.tw/Course/main/query/byKeywords?query=%E6%9F%A5%E8%A9%A2&keyword=&fall_spring=1&year=103&day=&d-49489-p=1&selectDept=&week=1"
+# change default language to Chinese
+RestClient.get "https://course.ncu.edu.tw/Course/main/lang", cookies: r.cookies
 
+(1..46).each do |page_number|
 	string = nil
 
 	if File.exist?("1031/#{page_number}.html")
@@ -13,7 +16,7 @@ courses = []
 	else
 		url = "https://course.ncu.edu.tw/Course/main/query/byKeywords?query=%E6%9F%A5%E8%A9%A2&keyword=&fall_spring=1&year=103&day=&d-49489-p=#{page_number}&selectDept=&week=1"
 
-		string = (RestClient.get url).to_s
+		string = (RestClient.get url, cookies: r.cookies).to_s
 		File.open("1031/#{page_number}.html", 'w') {|f| f.write(string)}
 	end
 
@@ -30,13 +33,31 @@ courses = []
     names.each {|d,i| d.strip!}
     names.each {|d| names.delete(d) if d.empty? }
 
+    times = datas[4] && datas[4].text && datas[4].search('br').each {|d| d.replace("\n")} && datas[4].text.strip.split("\n")
+    periods = []
+    if times
+      times.each do |time|
+        m = time.match(/(?<d>Mon|Tue|Wed|Thu|Fri|Sat|Sun)(?<p>\d+)\/(?<loc>.+)/)
+        if !!m
+          m[:p].split("").each do |period|
+            chars = []
+            chars << m[:d]
+            chars << period
+            chars << m[:loc]
+            periods << chars.join(',')
+          end
+        end
+      end
+    end
+
+
 		courses << {
 			code: datas[0] && datas[0].text,
 			name: names[1],
       english_name: names[0],
 			lecturer: datas[2] && datas[2].text && datas[2].text.strip,
 			credits: datas[3] && datas[3].text && datas[3].text.to_i,
-			classroom_time: datas[4] && datas[4].text && datas[4].text.strip,
+      periods: periods,
 			required: datas[5] && datas[5].text && datas[5].text.strip,
 			semester: datas[6] && datas[6].text && datas[6].text.strip,
 			outline: url,
